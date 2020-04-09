5c27cd5021ef8390b7a79012b137f8e1e18d3853
NickLewry <nicholas.lewry@icloud.com>
2020-04-09 17:38:07 +0100

    create basic home page

--- packages/app/App.tsx
+++ packages/app/App.tsx
@@ -1,19 +1,7 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React from 'react';
+
+import { HomePage } from './HomePage';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hey there!</Text>
-    </View>
-  );
+  return <HomePage />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ packages/app/HomePage.tsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import { Alert } from 'react-native';
+import styled from 'styled-components/native';
+
+export const HomePage: React.FC<any> = () => {
+  return (
+    <Container>
+      <Header></Header>
+      <Title>Socially Distant</Title>
+      <GamesContainer>
+        <Games>
+          <Game></Game>
+        </Games>
+        <GameActionsContainer>
+          <StartButtonContainer onPress={() => Alert.alert('Starting game...')}>
+            <ButtonText>New Game</ButtonText>
+          </StartButtonContainer>
+          <JoinButtonContainer onPress={() => Alert.alert('Joining game...')}>
+            <ButtonText>Join game</ButtonText>
+          </JoinButtonContainer>
+        </GameActionsContainer>
+      </GamesContainer>
+    </Container>
+  );
+};
+
+const Container = styled.View`
+  height: 100%;
+  display: flex;
+  align-items: center;
+`;
+
+const Header = styled.View`
+  margin: 40px 0;
+`;
+
+const Title = styled.Text`
+  font-size: 30px;
+  font-family: 'Verdana';
+`;
+
+const GamesContainer = styled.View`
+  margin: 60px 0;
+`;
+
+const Games = styled.View``;
+
+const Game = styled.View`
+  height: 300px;
+  width: 280px;
+  border: 1px solid black;
+  border-radius: 6px;
+`;
+
+const GameActionsContainer = styled.View`
+  display: flex;
+  align-items: center;
+  margin: 60px 0 20px;
+`;
+
+const ButtonContainer = styled.TouchableHighlight`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 60px;
+  width: 200px;
+  margin: 10px 0;
+  border: 1px solid lightgrey;
+  border-radius: 4px;
+`;
+
+const StartButtonContainer = styled(ButtonContainer)`
+  background: #ffbdbd;
+`;
+
+const JoinButtonContainer = styled(ButtonContainer)`
+  background: #c9c9ff;
+`;
+
+const ButtonText = styled.Text`
+  color: #fff;
+  font-family: 'Verdana-Bold';
+`;
